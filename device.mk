8629b5de6ac3990bcb0bf4332fd9b1be8d83af23
Mashopy <eliasgheeraert@gmail.com>
2026-04-23 18:00:15 +0200

    klee: Install mdota symlink
    Change-Id: Id9bffcc4b83c38acd2b4e17115920b8c6e8b5394

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
 $(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_PACKAGES += \
-    vendor_mdota_symlink
+    mdota_symlink
 
 PRODUCT_PACKAGES += \
     xiaomi-telephony-stub
