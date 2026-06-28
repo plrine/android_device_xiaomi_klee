5506b10e7d31118c933e7b2fb1a49948a9640fa8
Nicholas Lim <niclimcy@lineageos.org>
2026-06-29 02:22:00 +0800

    klee: Shim libsink-mtk
    Change-Id: I9a2be803ea8695c3bfc410397c17a4a583bf95d4

--- extract-files.py
+++ extract-files.py
@@ -257,6 +257,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libformatter.so', 'libformatter_mtk.so'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),
 }  # fmt: skip
