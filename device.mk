90340d8bf23a3365ebf1e4f217cd69dfb3f118db
Mashopy <eliasgheeraert@gmail.com>
2026-07-06 18:38:41 +0200

    klee: Declare wifi HAL wrapper struct is older than U QPR2
    Change-Id: I370e553846e493445a814edd1a7842cda75fb954

--- device.mk
+++ device.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # WiFi
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
