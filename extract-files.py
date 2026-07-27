3ea282b60b409bfbd086120207830f2ca4c80f0c
Waxaranai <waxaranai@gmail.com>
2026-07-27 04:12:26 +0000

    klee: Patch libsink-mtk with libaudioclient

--- extract-files.py
+++ extract-files.py
@@ -292,6 +292,12 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libformatter.so', 'libformatter_mtk.so'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
+    (
+        'system/bin/spkcal',
+        'system_ext/bin/spkcal_si_o12u',
+        'system_ext/lib64/libsink-mtk.so',
+    ): blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),
     (
