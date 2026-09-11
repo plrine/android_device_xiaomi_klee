c8c402787043569a0a10c0274afa7b8bc16eb3bd
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-09-11 16:47:56 +0000

    klee: Shim spkcal

--- extract-files.py
+++ extract-files.py
@@ -259,6 +259,11 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
+    (
+        'system/bin/spkcal',
+        'system_ext/bin/spkcal_si_o12u',
+    ): blob_fixup()
+        .add_needed('libaudiobase.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),
     (
